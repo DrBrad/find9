a61502b5b300112d0c94a7394112e0c020a3ba43
DrBrad <brad@bradeagle.com>
2025-02-04 01:05:54 -0700

    Type Handler remove

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crate::records::mx_record::MxRecord;
 use crate::records::ns_record::NsRecord;
 use crate::records::opt_record::OptRecord;
 use crate::records::ptr_record::PtrRecord;
-use records::inter::record_handler::RecordHandler;
+use crate::records::inter::record_handler::RecordHandler;
 use crate::records::soa_record::SoaRecord;
 use crate::records::txt_record::TxtRecord;
 use crate::utils::dns_query::DnsQuery;
