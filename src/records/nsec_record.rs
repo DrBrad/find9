2d32197eeca4a277937e92add920d4241fc9fea3
DrBrad <brad@bradeagle.com>
2025-02-07 00:07:05 -0700

    NS and NSEC records fixed

--- src/records/nsec_record.rs
+++ src/records/nsec_record.rs
@@ -32,20 +32,15 @@ impl DnsRecord for NsecRecord {
     fn encode(&self, label_map: &mut HashMap<String, usize>, off: usize) -> Result<Vec<u8>, String> {
         let mut buf = vec![0u8; 10];
 
-        buf[0] = (self.get_type().get_code() >> 8) as u8;
-        buf[1] = self.get_type().get_code() as u8;
+        buf.splice(0..2, self.get_type().get_code().to_be_bytes());
 
         let mut dns_class = self.dns_class.unwrap().get_code();
         if self.cache_flush {
             dns_class = dns_class | 0x8000;
         }
-        buf[2] = (dns_class >> 8) as u8;
-        buf[3] = dns_class as u8;
 
-        buf[4] = (self.ttl >> 24) as u8;
-        buf[5] = (self.ttl >> 16) as u8;
-        buf[6] = (self.ttl >> 8) as u8;
-        buf[7] = self.ttl as u8;
+        buf.splice(2..4, dns_class.to_be_bytes());
+        buf.splice(4..8, self.ttl.to_be_bytes());
 
         buf.extend_from_slice(&pack_domain(self.domain.as_ref().unwrap().as_str(), label_map, off+12));
 
@@ -70,8 +65,7 @@ impl DnsRecord for NsecRecord {
             }
         }
 
-        buf[8] = (buf.len()-10 >> 8) as u8;
-        buf[9] = (buf.len()-10) as u8;
+        buf.splice(8..10, ((buf.len()-10) as u16).to_be_bytes());
 
         Ok(buf)
     }
@@ -79,18 +73,14 @@ impl DnsRecord for NsecRecord {
     fn decode(buf: &[u8], off: usize) -> Self {
         let mut off = off;
 
-        let dns_class = ((buf[off] as u16) << 8) | (buf[off+1] as u16);
+        let dns_class = u16::from_be_bytes([buf[off], buf[off+1]]);
         let cache_flush = (dns_class & 0x8000) != 0;
         let dns_class = Some(DnsClasses::get_class_from_code(dns_class & 0x7FFF).unwrap());
-
-        let ttl = ((buf[off+2] as u32) << 24) |
-            ((buf[off+3] as u32) << 16) |
-            ((buf[off+4] as u32) << 8) |
-            (buf[off+5] as u32);
+        let ttl = u32::from_be_bytes([buf[off+2], buf[off+3], buf[off+4], buf[off+5]]);
 
         let (domain, length) = unpack_domain(buf, off+8);
 
-        let data_length = off+8+(((buf[off+6] as u16) << 8) | (buf[off+7] as u16)) as usize;
+        let data_length = off+8+u16::from_be_bytes([buf[off+6], buf[off+7]]) as usize;
         off += length+8;
 
         let mut rr_types = Vec::new();
